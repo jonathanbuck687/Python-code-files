2350b58ccb0d500d60d279db9ba59aa8b3e32891
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-09-01 11:02:13 -0600

    Add files via upload

---
+++ dimmable.py
@@ -0,0 +1,11 @@
+from gpiozero import PWMLED
+from time import sleep
+from gpiozero import RotaryEncoder
+
+led = PWMLED(18)
+
+rotor = RotaryEncoder(16, 20, wrap=True, max_steps=180)
+
+while True:
+	led.value = (rotor.steps/180)**2
+	sleep(1)
