7f2b1b031d2a300cc893610877926da9fa18240b
jonathanbuck687 <jonathanb138@lpsk12.org>
2026-09-03 10:28:49 -0600

    Delete classified.py

--- classified.py
+++
@@ -1,8 +0,0 @@
-target_words = ["james", "london", "mi6", "classified", "paris", "midnight", "nuclear", "asset"]
-text = input("Classifier: ")
-text = text.lower()
-for i in target_words:
-    if i in text:
-        index = text.find(i)
-        text = text[:index] + "[REDACTED]" + text[index + len(i):]
-print(text[0:1].upper() + text[1:])
